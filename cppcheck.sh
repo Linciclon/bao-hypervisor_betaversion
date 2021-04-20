3e5592f711b4be87c27ad1e23d9ec749747dc5ec
linciclon <a82273@alunos.uminho.pt>
2021-04-20 16:37:18 +0100

    test_jenkins 2

--- cppcheck.sh
+++ cppcheck.sh
@@ -9,6 +9,7 @@ then
   echo "files:"
   cppcheck --error-exitcode=1 --dump *.c
   python /home/joao/cppcheck-donate-cpu-workfolder/cppcheck/addons/misra.py --rule-texts=./rules.txt *.c.dump
+  
 else
   echo "THERE IS NO FILES IN THIS DIR" 
 fi
